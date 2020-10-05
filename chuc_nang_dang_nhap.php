390c7af7d93d8e0cda13a0b500797d42870db857
CongTrong <congtrong94@gmail.com>
2020-10-05 20:08:53 +0700

    add new file

---
+++ chuc_nang_dang_nhap.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<form method="post">
+    username:<input type="text" name="username" placeholder="username" /><br>
+    password:<input type="password" name="password" placeholder="password"/><br>
+    <button type="submit" value="sign in">submid</button>
+</form>
+<?php
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if ($username=='congtrong' && $password =='anhtrong'){
+        echo 'wellcome:  '.$username;
+    }else{
+        echo 'login error';
+    }
+}
+?>
+
+</body>
+</html>
